0424d15f6d30ae66ccb9017c7097bb7c72bc595f
Sean Lee <seanklee04@gmail.com>
2026-04-26 11:50:53 -0400

    Create legitimate_wget.sh

---
+++ legitimate/legitimate_wget.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+while true
+do 
+wget http://10.1.5.2/1.html -O /dev/null
+sleep 1
+wget http://10.1.5.2/2.html -O /dev/null
+sleep 1
+wget http://10.1.5.2/3.html -O /dev/null
+sleep 1
+wget http://10.1.5.2/4.html -O /dev/null
+sleep 1
+wget http://10.1.5.2/5.html -O /dev/null
+sleep 1
+wget http://10.1.5.2/6.html -O /dev/null
+sleep 1
+wget http://10.1.5.2/7.html -O /dev/null
+sleep 1
+wget http://10.1.5.2/8.html -O /dev/null
+sleep 1
+wget http://10.1.5.2/9.html -O /dev/null
+sleep 1
+wget http://10.1.5.2/10.html -O /dev/null
+sleep 1
+done
