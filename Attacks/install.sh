bc2be14836bdc8eb16551cc602a1655687f135c0
Sean Lee <seanklee04@gmail.com>
2026-04-26 13:09:59 -0400

    Update install.sh

--- Attacks/install.sh
+++ Attacks/install.sh
@@ -1,4 +1,14 @@
+#!/bin/bash
+echo "Insalling hping3"
 sudo apt-get install hping3
+echo "Done installing hping3"
+echo "======================================="
+echo "installing slowhttptest"
 sudo ap-get install slowhttptest
+echo "Done installing slowhttptest"
+echo "======================================="
+echo "installing slowloris"
 udo apt-get install git python3
 git clone https://github.com/gkbrk/slowloris.git
+echo "Done installing slowloris"
+echo "======================================="
