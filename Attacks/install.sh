f981f6a958748603b1d6414dc59cd7545e1624b1
Minh Nguyen <99856380+bogaktik@users.noreply.github.com>
2026-04-29 13:22:37 -0400

    Fix typo in install command for slowhttptest
    Corrected the command 'ap-get' to 'apt-get' for installing slowhttptest.

--- Attacks/install.sh
+++ Attacks/install.sh
@@ -4,7 +4,7 @@ sudo apt-get install hping3
 echo "Done installing hping3"
 echo "======================================="
 echo "installing slowhttptest"
-sudo ap-get install slowhttptest
+sudo apt-get install slowhttptest
 echo "Done installing slowhttptest"
 echo "======================================="
 echo "installing slowloris"
